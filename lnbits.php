c8f482156d44ecce623bc80b89e0635df14801ba
cryptoteun <cryptoteun@protonmail.com>
2024-03-08 11:48:21 +0200

    New release 0.7

--- lnbits.php
+++ lnbits.php
@@ -4,7 +4,7 @@ Plugin Name: Bitcoin Payment Gateway
 Plugin URI: https://github.com/lightningcheckout/wp-lnc-bitcoin/tree/lnc
 Forked from: https://github.com/lnbits/woocommerce-payment-gateway
 Description: Accept onchain bitcoin and bitcoin via the lightning network. Brought to you by Lightning Checkout.
-Version: 0.6
+Version: 0.7
 Author: Lightning Checkout
 Author URI: https://lightningcheckout.eu
 */
